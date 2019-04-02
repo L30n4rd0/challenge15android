44a5f0790d8f62c80c86f6230e050b2fdef889f3
leonardo souza <leonardo44ls@gmail.com>
2019-04-02 20:28:38 -0300

    Layout converted to ConstraintLayout

--- app/src/main/java/com/leosouzatech/challenge15android/MainActivity.java
+++ app/src/main/java/com/leosouzatech/challenge15android/MainActivity.java
@@ -2,6 +2,7 @@ package com.leosouzatech.challenge15android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView teste = findViewById(R.id.imageViewHorizontalDown);
     }
 }
